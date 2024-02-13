1afed74cd4bea52225cb465b5ee090d7ed5cd022
Yuri Zhulev <yuri.zhulyov@gmail.com>
2024-02-13 12:00:26 +0300

    Init main add. Add routes for index, about pages

--- main/views.py
+++ main/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"title": "Home", "content": "Online store main page - HOME"}
+    context = {"title": "Home - Main page", "content": "Online Guitar Store"}
 
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "About page",
+        "content": "About Page",
+        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, ipsam.",
+    }
+
+    return render(request, "main/about.html", context)
